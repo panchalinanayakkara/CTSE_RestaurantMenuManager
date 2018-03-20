73da9fbb97c27322d8c0c5c665da80f18ff40f1a
Panchali Nanayakkara <panchali_nanayakkara@hotmail.com>
2018-03-20 16:03:33 +0530

    Initialized the main UI of the application

--- app/src/main/java/com/panchalinanayakkara/dev/restaurantmenumanager/MainActivity.java
+++ app/src/main/java/com/panchalinanayakkara/dev/restaurantmenumanager/MainActivity.java
@@ -2,12 +2,38 @@ package com.panchalinanayakkara.dev.restaurantmenumanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        title = (TextView)findViewById(R.id.txtTitle);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
